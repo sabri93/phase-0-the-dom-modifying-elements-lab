d3f0e12242473d6dcdeb3a5610dc689a4da03755
sabrina <sabrinadjebbar93@gmail.com>
2022-04-20 17:44:18 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+ main.remove();
+
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.innerHTML="YOUR-NAME is the champion" 
+
+document.body.append(newHeader.id = 'victory');
+
+
+
+ 
